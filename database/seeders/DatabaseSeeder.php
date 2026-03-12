3828b16cb8a6da4556b5c259991803ff5be6f55c
AZIZABADA10 <abadaaziz288@gmail.com>
2026-03-12 02:27:59 +0000

    Feat(Auth): call RoleSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(RoleSeeder::class);
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // On ne crée pas le test user ici car le premier inscrit sera l'admin
     }
 }
