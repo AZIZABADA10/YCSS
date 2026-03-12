ef0e8c908654e9023151abef3411af5cb7902598
AZIZABADA10 <abadaaziz288@gmail.com>
2026-03-12 05:13:26 +0000

    feat: modification sur les routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.auth');
 });
+
+
+ 
+Route::get('/auth', [AuthController::class, 'showAuthForm'])->name('auth.show');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+Route::get('/dashboard', [AuthController::class, 'showDashboard'])->name('dashboard');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
